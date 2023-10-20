76b3df13409345eabfa4a3620a9387c0d778b33f
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-10-20 21:19:59 +0300

    docker

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python
+
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWEITEBYTECODE 1
+
+WORKDIR /task
+
+COPY pyproject.toml poetry.lock ./
+
+RUN pip install poetry && \
+    poetry config virtualenvs.create false && \
+    poetry install --no-root
+
+EXPOSE 8000
+
+COPY . .
--- tasks/models.py
+++ tasks/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=64)
 
+    def __str__(self):
+        return f"ID: {self.pk} | Title: {self.title}"
 
 # class TaskStatus(models.Model):
 #     status = models.CharField(max_length=16)
