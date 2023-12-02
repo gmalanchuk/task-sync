a7865461f01f8771e05c1ce71a4e3567e4b8b019
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-12-02 11:25:57 +0200

    remove greeter protos

--- tasks/grpc_services/permission.py
+++ tasks/grpc_services/permission.py
@@ -6,9 +6,7 @@ from tasks.protos.permission_pb2_grpc import PermissionStub
 
 
 def check_permission(token: str) -> None | str:
-    with grpc.insecure_channel(
-        f"{AUTHENTICATION_SERVICE_DOMAIN}:{GRPC_PORT}"
-    ) as channel:  # todo вынести порт и домен в .env
+    with grpc.insecure_channel(f"{AUTHENTICATION_SERVICE_DOMAIN}:{GRPC_PORT}") as channel:
         stub = PermissionStub(channel)
         try:
             response = stub.CheckPermission(PermissionRequest(token=token))
