9019115def3edbd5755d1dc00341b61deeaa3615
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-12-04 19:33:03 +0200

    refactoring

--- tasks/permissions/checks.py
+++ tasks/permissions/checks.py
@@ -12,36 +12,29 @@ class Checks:
         token = request.COOKIES.get("access_token")
         return check_permission(token)
 
-    def is_authenticated(self, request: Any, *args: Any, **kwargs: Any) -> CustomAPIException | bool:
+    def is_authenticated(self, request: Any, *args: Any, **kwargs: Any) -> CustomAPIException | str | None:
         role_or_error_message = self.__permission_check(request)
         if isinstance(role_or_error_message, dict):
             raise CustomAPIException(detail=role_or_error_message["data"], status_code=role_or_error_message["status"])
 
-        return True
+        return role_or_error_message
 
-    def is_staff(self, request: Any, *args: Any, **kwargs: Any) -> CustomAPIException | bool:
-        role_or_error_message = self.__permission_check(request)
-        if isinstance(role_or_error_message, dict):
-            raise CustomAPIException(detail=role_or_error_message["data"], status_code=role_or_error_message["status"])
+    def is_staff(self, request: Any, *args: Any, **kwargs: Any) -> CustomAPIException | None:
+        role = self.is_authenticated(request)
 
-        if role_or_error_message not in ("staff", "admin"):
+        if role not in ("staff", "admin"):
             raise CustomAPIException(
                 detail="Permission Denied: You do not have sufficient privileges to perform this action",
                 status_code=status.HTTP_401_UNAUTHORIZED,
             )
+        return None
 
-        return True
+    def is_admin(self, request: Any, *args: Any, **kwargs: Any) -> CustomAPIException | None:
+        role = self.is_staff(request)
 
-    def is_admin(self, request: Any, *args: Any, **kwargs: Any) -> CustomAPIException | bool:
-        # TODO все права строятся на предыдущих
-        role_or_error_message = self.__permission_check(request)
-        if isinstance(role_or_error_message, dict):
-            raise CustomAPIException(detail=role_or_error_message["data"], status_code=role_or_error_message["status"])
-
-        if role_or_error_message != "admin":
+        if role != "admin":
             raise CustomAPIException(
                 detail="Permission Denied: You do not have sufficient privileges to perform this action",
                 status_code=status.HTTP_401_UNAUTHORIZED,
             )
-
-        return True
+        return None
