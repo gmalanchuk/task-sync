040de10f0aad358b20a13dbf6786e89f0d35f19b
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-12-14 22:51:32 +0200

    get_serializer_class

--- tasks/serializers/board_serializer.py
+++ tasks/serializers/board_serializer.py
@@ -3,7 +3,14 @@ from rest_framework.serializers import ModelSerializer
 from tasks.models.board_model import Board
 
 
-class BoardSerializer(ModelSerializer):
+class BoardListRetrieveSerializer(ModelSerializer):
     class Meta:
         model = Board
         fields = "__all__"
+
+
+class BoardPutPatchPostSerializer(ModelSerializer):
+    class Meta:
+        model = Board
+        fields = "__all__"
+        read_only_fields = ("owner_id",)
--- tasks/views/board_view.py
+++ tasks/views/board_view.py
@@ -5,14 +5,19 @@ from rest_framework.viewsets import ModelViewSet
 
 from tasks.models import Board
 from tasks.permissions import is_admin_or_owner_user, is_authenticated_user
-from tasks.serializers import BoardSerializer
+from tasks.serializers import BoardListRetrieveSerializer, BoardPutPatchPostSerializer
 
 
 class BoardViewSet(ModelViewSet):
     queryset = Board.objects.all()
-    serializer_class = BoardSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ("owner_id",)
+
+    def get_serializer_class(self) -> type[BoardPutPatchPostSerializer] | type[BoardListRetrieveSerializer]:
+        if self.request.method in ("PUT", "PATCH", "POST"):
+            return BoardPutPatchPostSerializer
+        else:
+            return BoardListRetrieveSerializer
 
     @is_authenticated_user
     def create(self, request: Any, *args: Any, **kwargs: Any) -> Any:
