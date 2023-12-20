3dfba85ab0f874cc06eccf9a2f5a9644605f6481
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-12-20 23:33:56 +0200

    bugfix

--- tasks/rabbitmq/notifications/event_notification.py
+++ tasks/rabbitmq/notifications/event_notification.py
@@ -1,6 +1,7 @@
 from typing import Any, Callable
 
 from django.db.models import QuerySet
+from django.shortcuts import get_object_or_404
 
 from config.settings import logger
 from tasks.grpc_services.user import get_user_info
@@ -13,27 +14,25 @@ def event_notification(queryset: QuerySet) -> Any:
     def decorator(view_func: Callable) -> Callable:
         def wrapper(obj: Any, request: Any, *args: Any, **kwargs: Any) -> Any:
             if request.method in ("POST", "PUT", "PATCH", "DELETE"):
+                model = queryset.model
+                obj_model = get_object_or_404(model, id=request.path.split("/")[-2])
                 response = view_func(obj, request, *args, **kwargs)
-                response_data = response.data
 
                 access_token = request.COOKIES.get("access_token")
                 user_info = get_user_info(token=access_token)
-                title = response_data["title"]
 
                 is_owner = False
-                if response_data["owner_id"] == user_info["user_id"]:
+                if obj_model.owner_id == user_info["user_id"]:
                     is_owner = True
-
-                model_name = queryset.model.__name__.lower()
 
                 producer_event_notification(
                     username=user_info["username"],
-                    title=title,
+                    title=obj_model.title,
                     email=user_info["email"],
                     role=user_info["role"],
                     is_owner=is_owner,
                     http_method=request.method,
-                    model_name=model_name,
+                    model_name=model.__name__.lower(),
                 )
 
                 return response
